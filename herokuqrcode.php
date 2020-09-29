828ab89cb51d822f7f78e1afa5d1b8053b868b52
YOUR_USERNAME <your_email_address@example.com>
2020-09-30 01:47:28 +0700

    ddd

--- herokuqrcode.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$ch = curl_init('https://qrackajack.expeditedaddons.com/?api_key=' . getenv('QRACKAJACK_API_KEY') . '&bg_color=%23ffffff&content=http%3A%2F%2Fexample.org&fg_color=%23000000&height=256&width=256');
-
-$response = curl_exec($ch);
-curl_close($ch);
-
-var_dump($response);
