36bf61ec10eedffba3a7bd79bfaefdf08b547db4
YOUR_USERNAME <your_email_address@example.com>
2020-10-01 19:26:25 +0700

    ssss

--- index.php
+++ index.php
@@ -480,7 +480,7 @@ if($arrJson['events'][0]['message']['text'] == "ID" ){
   $payment_type_name1 =urlencode($response->payment_type_name);
   $student_name1 =urlencode($response->student_name);
   // https://ff8c0a85c27f.ngrok.io/LineQrcode/phpqrcode/QRC/img_get.php?ref1=$response->text_ref1&&ref2=$response->text_ref2&&name=$name
-  $QRC_PNG="https://select2web-autobot-bantan.herokuapp.com/LineQrcode/phpqrcode/QRC/bg_loan.png";
+  $QRC_PNG="http://103.80.49.95:82/LineQrcode/phpqrcode/QRC/bg_loan.png";
    $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "image";
