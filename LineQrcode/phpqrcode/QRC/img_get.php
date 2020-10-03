515813926bfdb742565529d7d2607f4e19b306e0
HANEEF <erpsysware.com>
2020-10-03 20:54:11 +0700

    hjj

---
+++ LineQrcode/phpqrcode/QRC/img_get.php
@@ -0,0 +1,68 @@
+<?php
+
+//--------------ส่งต่อ QRC ----------------
+$ref1=$_REQUEST["ref1"]; //use
+$ref2=$_REQUEST["ref2"];
+$price=$_REQUEST["price"]; //use
+// ---------------- BG -----------------------
+
+$payment_type_name=$_REQUEST["payment_type_name"];
+$student_name=$_REQUEST["student_name"];
+
+// ---------------- BG -----------------------
+$payment_type_name1 = urldecode($payment_type_name); // url ตัวเปลี่ยนภาษาไทย
+$student_name1 = urldecode($student_name);  // url ตัวเปลี่ยนภาษาไทย
+
+
+
+$image = imagecreatetruecolor(1792,2800);
+$black = imagecolorallocate($image,0,0,0);
+imagefill($image,0,0,$black);
+
+// load image from file and draw it onto black image;
+// for loading PNG, use imagecreatefrompng()
+$overlayImage_bg = imagecreatefrompng('bg_saving.png');
+
+
+// $src = "https://ff8c0a85c27f.ngrok.io/LineQrcode/phpqrcode/QRC/bg_member.png";
+// $overlayImage_bg = imagecreatefromstring(file_get_contents($src));
+
+
+$src = "http://103.80.49.95:82/LineQrcode/phpqrcode/QRC/QR_Code.php";
+$overlayImage_qr_code = imagecreatefromstring(file_get_contents($src));
+
+$overlayImage_qr_code= imagescale($overlayImage_qr_code , 650, 650); 
+
+//$overlayImage_qr_code = imagecreatefromfile('https://ff8c0a85c27f.ngrok.io/LineQrcode/phpqrcode/QRC/bg_member.png');
+#imagecopy($image, $overlayImage, 10, 10, 0, 0, imagesx($overlayImage), imagesy($overlayImage));
+imagecopy($image, $overlayImage_bg, 0, 0, 0, 0, imagesx($overlayImage_bg), imagesy($overlayImage_bg)); 
+imagecopy($image, $overlayImage_qr_code, 580, 1000, 0, 0, 650, 650); 
+$font = imageloadfont('./Hollow_8x16_LE.gdf'); 
+$black = imagecolorallocate($image, 0, 0, 0);
+//imagestring ($image , 5 , 0, 0 , 'HelloWorld999999999999999999999999999999999' , $black  ) ;
+
+// $text = 'Testing...66666666666666666666666666666666666666666666666666';
+// Replace path by your own font path
+$font = '/opt/lampp/htdocs/class/securimage/ttf/TATSana Chon-Reg.otf';
+$grey = imagecolorallocate($im, 128, 128, 128);
+imagettftext($image, 65, 0,900,1950, $grey, $font,"$var");     //name
+imagettftext($image, 48, 0,900,2025, $grey, $font,"$account_no");  //REF1
+// imagettftext($image, 48, 0,1000,2100, $grey, $font,"005-01-3846");  //REF2   
+
+
+
+
+
+
+//imagettftext ( $image , float $size , float $angle , int $x , int $y , int $color , string $fontfile , string $text ) : array
+// imagecopy ( resource $dst_im , resource $src_im , int $dst_x , int $dst_y , int $src_x , int $src_y , int $src_w , int $src_h ) : bool
+// Copy a part of src_im onto dst_im starting at the x,y coordinates src_x, src_y with a width of src_w and a height of src_h. The portion defined will be copied onto the x,y coordinates, dst_x and dst_y.
+
+
+
+
+
+header("Content-Type: image/png");
+imagepng($image);
+
+?>
