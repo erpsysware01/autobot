9b8cf825a8b6bea07099bc4d7aa2416246d2d34c
YOUR_USERNAME <your_email_address@example.com>
2020-10-06 20:38:07 +0700

    sss

--- LineQrcode/phpqrcode/QRC/img_get.php
+++ LineQrcode/phpqrcode/QRC/img_get.php
@@ -31,7 +31,7 @@ imagefill($image,0,0,$black);
    // $filename = substr($ref2, 4 ,8);
    // $BG_picture = '/BG/$filename.png';
    // $overlayImage_bg = imagecreatefrompng($BG_picture);
-   $overlayImage_bg = imagecreatefrompng('/BG/0010000.png');
+   $overlayImage_bg = imagecreatefrompng('BG/0010000.png');
 
 // $src = "https://ff8c0a85c27f.ngrok.io/LineQrcode/phpqrcode/QRC/bg_member.png";
 // $overlayImage_bg = imagecreatefromstring(file_get_contents($src));
