d94f22b6652029953864564504ece3c0875a94ba
YOUR_USERNAME <your_email_address@example.com>
2020-10-07 00:27:02 +0700

    ddd

--- LineQrcode/phpqrcode/QRC/QRcode.php
+++ LineQrcode/phpqrcode/QRC/QRcode.php
@@ -3,6 +3,8 @@ $ref1=$_REQUEST["ref1"];
 $ref2=$_REQUEST["ref2"];
 $price=$_REQUEST["price"];
 
+$price1 = round($price,0);
+
 
  $Bantan="|099400009375600";
 //  $ref1="1800801571929";
@@ -11,6 +13,6 @@ $price=$_REQUEST["price"];
 //  $zero='00';
 
 include('../qrlib.php'); 
-QRcode::png($Bantan."\n".$ref1."\n".$ref2."\n".$price);
+QRcode::png($Bantan."\n".$ref1."\n".$ref2."\n".$price1.'00');
 
 ?>
