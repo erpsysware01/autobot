172fec4c30ee51fbfa380591ec43a6d125285e37
YOUR_USERNAME <your_email_address@example.com>
2020-10-07 23:42:17 +0700

    sss

--- index.php
+++ index.php
@@ -689,18 +689,18 @@ if($arrJson['events'][0]['message']['text'] == "ID" | $arrJson['events'][0]['mes
     $school_name1 =urlencode($response->school_name);
      
   
-   // $QRC_PNG="https://select2web-autobot-bantan.herokuapp.com/LineQrcode/phpqrcode/QRC/BG_QR_SSD.php?ref1=$response->ref1&&ref2=$response->ref2&&price=$response->price&&payment_type_name=$payment_type_name1&&student_name=$student_name1&&class_name=$class_name1&&school_name=$school_name1";
-    // $arrPostData = array();
-    // $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-    // $arrPostData['messages'][0]['type'] = "image";
-    // $arrPostData['messages'][0]['originalContentUrl'] = $QRC_PNG;
-    // $arrPostData['messages'][0]['previewImageUrl'] =  $QRC_PNG;
+   $QRC_PNG="https://select2web-autobot-bantan.herokuapp.com/LineQrcode/phpqrcode/QRC/BG_QR2.php?ref1=$response->ref1&&ref2=$response->ref2&&price=$response->price&&payment_type_name=$payment_type_name1&&student_name=$student_name1&&class_name=$class_name1&&school_name=$school_name1";
+    $arrPostData = array();
+    $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+    $arrPostData['messages'][0]['type'] = "image";
+    $arrPostData['messages'][0]['originalContentUrl'] = $QRC_PNG;
+    $arrPostData['messages'][0]['previewImageUrl'] =  $QRC_PNG;
 
   
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "$response->student_name"; 
+  // $arrPostData = array();
+  // $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  // $arrPostData['messages'][0]['type'] = "text";
+  // $arrPostData['messages'][0]['text'] = "$response->student_name"; 
 
   
 
