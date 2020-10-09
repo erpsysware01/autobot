9e1bf8a441ad1874f4ea3d93dce03f0044a80842
YOUR_USERNAME <your_email_address@example.com>
2020-10-09 23:27:38 +0700

    sss

--- LineQrcode/phpqrcode/QRC/BG_QR2.php
+++ LineQrcode/phpqrcode/QRC/BG_QR2.php
@@ -68,8 +68,10 @@ imagettftext($image, 50, 0,750,1880, $grey, $font,"$payment_type_name1");  //pay
 imagettftext($image, 50, 0,610,2000, $grey, $font,"$school_name1"); //school_name
 imagettftext($image, 70, 0,720,2230, $grey, $font,"$price"); //price
 
-if( $payment_type_name1 != 'ค่าธรรมเนียมเทอม 1' | $payment_type_name1 != 'ค่าธรรมเนียมเทอม 2'){
+if( $payment_type_name1 == 'ค่าธรรมเนียมรายเดือน' | $payment_type_name1 == 'ค่าเรียนหลักสูตร IP '| $payment_type_name1 == 'ค่าใช้จ่ายส่วนตัวรายวัน' | $payment_type_name1 == 'ค่าใช้จ่ายอื่นๆ'| $payment_type_name1 == 'สนับสนุนทุนการศึกษา'| $payment_type_name1 == 'สนับสนุนกองทุนวากัฟ'| $payment_type_name1 == 'ค่าบำรุงสมาคมศิษย์เก่า'| $payment_type_name1 == 'สนับสนุนภารกิจ สถาบันสุรินทร์ พิศสุวรรณ'){
+
    imagettftext($image, 30, 0,650,2350, $grey, $font,"สามารถบันทึกเก็บไว้ใช้ในครั้งต่อไป"); 
+
     }
    
 
